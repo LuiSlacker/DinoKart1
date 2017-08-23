e4f96b8f229d643bec539a850708b9a8152c828c
saidc <said-jo@hotmail.com>
2017-08-23 14:59:30 -0500

    add sound

---
+++ Assets/Scripts/backgroundsound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundsound : MonoBehaviour {
+	public AudioClip clip;
+	public AudioSource music;
+	// Use this for initialization
+	void Start () {
+		this.music.clip = clip;
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			music.Play();
+		}
+	}
+
+}
